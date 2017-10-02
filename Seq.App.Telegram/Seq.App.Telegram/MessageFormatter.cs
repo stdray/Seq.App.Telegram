86a8cdb0c95c6e9a23f79a212f3eb13787e205e2
Arseniy Karpov <stdray@3po.su>
2017-10-02 18:39:18 +0300

    Add implemenration for TelegramReactor

---
+++ Seq.App.Telegram/Seq.App.Telegram/MessageFormatter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+using Serilog;
+
+namespace Seq.App.Telegram
+{
+    public class MessageFormatter
+    {
+        static readonly Regex PlaceholdersRegex = new Regex("(\\[(?<key>[^\\[\\]]+?)(\\:(?<format>[^\\[\\]]+?))?\\])", RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        
+        public MessageFormatter(ILogger log, string baseUrl, string messageTamplate)
+        {
+            Log = log;
+            MessageTamplate = messageTamplate ?? "[RenderedMessage]";
+            BaseUrl = baseUrl;
+        }
+
+        public ILogger Log { get; }
+        public string MessageTamplate { get; }
+        public string BaseUrl { get; }
+
+        public string GenerateMessageText(Event<LogEventData> evt)
+        {
+            return $"{SubstitutePlaceholders(MessageTamplate, evt)} (<{BaseUrl}/#/events?filter=@Id%20%3D%3D%20%22{evt.Id}%22&show=expanded|View on Seq>)";
+        }
+
+        string SubstitutePlaceholders(string messageTemplateToUse, Event<LogEventData> evt, bool addLogData = true)
+        {
+            var data = evt.Data;
+            var eventType = evt.EventType;
+            var level = data.Level;
+
+            var placeholders = data.Properties?.ToDictionary(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase) 
+                ?? new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+            if (addLogData)
+            {
+                AddValueIfKeyDoesntExist(placeholders, "Level", level);
+                AddValueIfKeyDoesntExist(placeholders, "EventType", eventType);
+                AddValueIfKeyDoesntExist(placeholders, "RenderedMessage", data.RenderedMessage);
+            }
+            return PlaceholdersRegex.Replace(messageTemplateToUse, m =>
+            {
+                var key = m.Groups["key"].Value.ToLower();
+                var format = m.Groups["format"].Value;
+                return placeholders.ContainsKey(key) ? FormatValue(placeholders[key], format) : m.Value;
+            });
+        }
+
+        string FormatValue(object value, string format)
+        {
+            var rawValue = value?.ToString() ?? "(Null)";
+
+            if (string.IsNullOrWhiteSpace(format))
+                return rawValue;
+            try
+            {
+                return string.Format(format, rawValue);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Could not format message: {value} {format}", value, format);
+            }
+
+            return rawValue;
+        }
+
+        static void AddValueIfKeyDoesntExist(IDictionary<string, object> placeholders, string key, object value)
+        {
+            var loweredKey = key.ToLower();
+            if (!placeholders.ContainsKey(loweredKey))
+                placeholders.Add(loweredKey, value);
+        }
+    }
+}
---
+++ Seq.App.Telegram/Seq.App.Telegram/TelegramReactor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.Http;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+using Telegram.Bot;
+
+namespace Seq.App.Telegram
+{
+    [SeqApp("Telegram notifier", Description = "Sends messages matching a view to Telegram.")]
+    public class TelegramReactor : Reactor, ISubscribeTo<LogEventData>
+    {
+        [SeqAppSetting(
+            DisplayName = "Bot authentication token",
+            HelpText = "https://core.telegram.org/bots/api#authorizing-your-bot")]
+        public string BotToken { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Group chat identifier",
+            HelpText = "Unique identifier for your group chat")]
+        public int ChatId { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Seq Base URL",
+            HelpText = "Used for generating perma links to events in Telegram messages.",
+            IsOptional = true)]
+        public string BaseUrl { get; set; }
+
+        [SeqAppSetting(
+            HelpText = "The message template to use when writing the message to Telegram. Refer to https://tlgrm.ru/docs/bots/api#formatting-options for formatting options. Event property values can be added in the format [PropertyKey]. The default is \"[RenderedMessage]\"",
+            IsOptional = true)]
+        public string MessageTemplate { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Suppression time (minutes)",
+            IsOptional = true,
+            HelpText = "Once an event type has been sent to Telegram, the time to wait before sending again. The default is zero.")]
+        public int SuppressionMinutes { get; set; } = 0;
+
+        readonly HttpClientHandler _httpClientHandler = new HttpClientHandler();
+        readonly ConcurrentDictionary<uint, DateTime> _lastSeen = new ConcurrentDictionary<uint, DateTime>();
+
+        public void On(Event<LogEventData> evt)
+        {
+            if (!CanSend(evt))
+                return;
+            using (var http = new HttpClient(_httpClientHandler, false))
+            {
+                var formatter = new MessageFormatter(Log, GetBaseUri(), MessageTemplate);
+                var message = formatter.GenerateMessageText(evt);
+                var telegram = new TelegramBotClient(BotToken, http);
+                telegram.SendTextMessageAsync(ChatId, message).Wait();
+            }
+        }
+
+        bool CanSend(Event<LogEventData> evt)
+        {
+            if (SuppressionMinutes < 1)
+                return true;
+            var canSend = false;
+            _lastSeen.AddOrUpdate(
+                key: evt.EventType,
+                addValueFactory: type =>
+                {
+                    canSend = true;
+                    return DateTime.Now;
+                },
+                updateValueFactory: (type, lastTime) =>
+                {
+                    var now = DateTime.Now;
+                    if ((now - lastTime).TotalMinutes > SuppressionMinutes)
+                    {
+                        canSend = true;
+                        return now;
+                    }
+                    return lastTime;
+                });
+            return canSend;
+        }
+
+        string GetBaseUri() => BaseUrl ?? Host.ListenUris.FirstOrDefault();
+    }
+
+
+}
