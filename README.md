2b85a167092f5bdf5e8efa78f2be2dd365e66515
Arseniy Karpov <stdray@3po.su>
2019-10-17 00:53:02 +0300

    Switch to .netstandart package (#5)
    Switch project to netstandart. Add icon file. Add appveyor build configuration. Add GitVersion config. Add Socks5 proxy support.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Seq.App.Telegram
-An app for Seq (http://getseq.net) that forwards messages to Telegram group chat. 
+An app for Seq (http://getseq.net) that forwards messages to Telegram group chat. Socks5 proxy supported.
 
 [![NuGet](https://img.shields.io/nuget/v/Seq.App.Telegram.svg?style=flat-square)](https://www.nuget.org/packages/Seq.App.Telegram/)
 
 ### In order to use Seq.App.Telegram you will need:
 * **Bot authentication token**. You can use existing bot's token or create a new one. Refer to docs at https://core.telegram.org/bots/api#authorizing-your-bot.
-* **Chat id**. Invite [@ShowJsonBot](https://telegram.me/ShowJsonBot) into your chat and it will send you a message. Copy `id` value from `chat` section including leading minus.
+* **Chat id**. Invite [@ShowJsonBot](https://telegram.me/ShowJsonBot) or [@RawDataBot](https://telegram.me/RawDataBot) into your chat and it will send you a message. Copy `id` value from `chat` section including leading minus.
 	`chat` section example:
 	```json
 	 "chat": {
--- src/Seq.App.Telegram/TelegramReactor.cs
+++ src/Seq.App.Telegram/TelegramReactor.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using MihaZupan;
 using Seq.Apps;
 using Seq.Apps.LogEvents;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 
 namespace Seq.App.Telegram
 {
     [SeqApp("Telegram notifier", Description = "Sends messages matching a view to Telegram.")]
-    public class TelegramReactor : Reactor, ISubscribeTo<LogEventData>
+    public class TelegramReactor : SeqApp, ISubscribeToAsync<LogEventData>
     {
         [SeqAppSetting(
             DisplayName = "Bot authentication token",
@@ -24,7 +24,7 @@ namespace Seq.App.Telegram
 
         [SeqAppSetting(
             DisplayName = "Seq Base URL",
-            HelpText = "Used for generating perma links to events in Telegram messages.",
+            HelpText = "Used for generating permalinks to events in Telegram messages.",
             IsOptional = true)]
         public string BaseUrl { get; set; }
 
@@ -39,20 +39,46 @@ namespace Seq.App.Telegram
             HelpText = "Once an event type has been sent to Telegram, the time to wait before sending again. The default is zero.")]
         public int SuppressionMinutes { get; set; } = 0;
 
-        readonly HttpClient _http = new HttpClient();
+        [SeqAppSetting(DisplayName = "Socks5 proxy host name", IsOptional = true)]
+        public string Socks5ProxyHost { get; set; }
+
+        [SeqAppSetting(DisplayName = "Socks5 proxy port", IsOptional = true)]
+        public int Socks5ProxyPort { get; set; }
+
+        [SeqAppSetting(DisplayName = "Socks5 proxy username", IsOptional = true)]
+        public string Socks5ProxyUserName { get; set; }
+
+        [SeqAppSetting(DisplayName = "Socks5 proxy password", IsOptional = true, InputType = SettingInputType.Password)]
+        public string Socks5ProxyPassword { get; set; }
+
+        readonly Lazy<TelegramBotClient> _telegram;
+
+        public TelegramReactor()
+        {
+            _telegram = new Lazy<TelegramBotClient>(CreateTelegramBotClient, LazyThreadSafetyMode.ExecutionAndPublication);
+        }
+
+        TelegramBotClient CreateTelegramBotClient()
+        {
+            if (string.IsNullOrEmpty(Socks5ProxyHost))
+                return new TelegramBotClient(BotToken);
+            var proxy = string.IsNullOrEmpty(Socks5ProxyUserName)
+                ? new HttpToSocks5Proxy(Socks5ProxyHost, Socks5ProxyPort)
+                : new HttpToSocks5Proxy(Socks5ProxyHost, Socks5ProxyPort, Socks5ProxyUserName, Socks5ProxyPassword);
+            return new TelegramBotClient(BotToken, proxy);
+        }
 
         readonly Throttling<uint> _throttling = new Throttling<uint>();
 
-        public void On(Event<LogEventData> evt)
+        string GetBaseUri() => (BaseUrl ?? Host.BaseUri).TrimEnd('/');
+
+        public async Task OnAsync(Event<LogEventData> evt)
         {
             if (!_throttling.TryBegin(evt.EventType, TimeSpan.FromMinutes(SuppressionMinutes)))
                 return;
             var formatter = new MessageFormatter(Log, GetBaseUri(), MessageTemplate);
             var message = formatter.GenerateMessageText(evt);
-            var telegram = new TelegramBotClient(BotToken, _http);
-            Task.Run(() => telegram.SendTextMessageAsync(ChatId, message, ParseMode.Markdown));
+            await _telegram.Value.SendTextMessageAsync(ChatId, message, ParseMode.Markdown);
         }
-
-        string GetBaseUri() => BaseUrl ?? Host.ListenUris.FirstOrDefault();
     }
 }
